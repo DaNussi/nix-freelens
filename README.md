f708e839bafe978e5f22a46336f7a52ca2626a2b
DaNussi <info@daniel-nussbaum.at>
2026-04-23 21:58:19 +0200

    aarch64

--- README.md
+++ README.md
@@ -5,11 +5,7 @@ Nix flake for packaging Freelens - Free IDE for Kubernetes.
 ## Quick Start
 
 ```bash
-# Build package
-nix build
-
-# Run Freelens
-nix run
+nix run 'github:DaNussi/nix-freelens'
 ```
 
 ## Platforms
@@ -17,4 +13,6 @@ nix run
 - x86_64-linux
 - aarch64-linux
 
-Automatically downloads the correct pre-built binary for your architecture.
+## Thanks
+
+Thank you to the Freelens community for building this amazing Kubernetes IDE!
--- flake.nix
+++ flake.nix
@@ -2,50 +2,64 @@
   description = "Freelens - Free IDE for Kubernetes";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-    packages.x86_64-linux.default = self.packages.x86_64-linux.freelens;
+  outputs = inputs@{ flake-parts, ... }: let
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+  in flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [];
+      systems = systems;
+      perSystem = { config, self', inputs', pkgs, system, ... }: let
+            version = "1.8.1";
+            pname = "freelens";
 
-    packages.x86_64-linux.freelens = pkgs.stdenv.mkDerivation rec {
-      pname = "freelens";
-      version = "1.8.1";
-      
-      src = pkgs.fetchurl {
-        url = "https://github.com/freelensapp/freelens/releases/download/v${version}/Freelens-${version}-linux-amd64.deb";
-        sha256 = "4b5bc4c87dd26251479157216a0f08baf815279e0e439b697de457e86839b748";
+
+            srcData = if pkgs.system == "aarch64-linux" then {
+              url = "https://github.com/freelensapp/freelens/releases/download/v${version}/Freelens-${version}-linux-arm64.deb";
+              sha256 = "394ae4ccc61fdf53e9a200c432d73d2ee7b497adee0b422c8d4138a77437123a";
+            } else if pkgs.system == "x86_64-linux" then {
+              url = "https://github.com/freelensapp/freelens/releases/download/v${version}/Freelens-${version}-linux-amd64.deb";
+              sha256 = "4b5bc4c87dd26251479157216a0f08baf815279e0e439b697de457e86839b748";
+            } else {};
+            
+        in {
+          packages.default = self'.packages.freelens;
+
+          packages.freelens = pkgs.stdenv.mkDerivation rec {
+            inherit pname version;
+            
+            src = pkgs.fetchurl srcData;
+
+            nativeBuildInputs = with pkgs; [
+              dpkg
+              autoPatchelfHook
+            ];
+
+            buildInputs = with pkgs; [
+              mesa
+              gtk3
+              nss
+              alsa-lib
+            ];
+
+            unpackPhase = "true";
+
+            installPhase = ''
+              mkdir -p $out/bin
+              dpkg -x $src $out
+              ln -s $out/opt/Freelens/freelens $out/bin/freelens
+            '';
+
+            meta = with pkgs.lib; {
+              description = "Freelens - Free IDE for Kubernetes";
+              homepage = "https://freelens.app";
+              license = licenses.mit;
+              maintainers = [];
+              platforms = systems;
+            };
+          };
       };
-
-      nativeBuildInputs = with pkgs; [
-        dpkg
-        autoPatchelfHook
-      ];
-
-      buildInputs = with pkgs; [
-        mesa
-        gtk3
-        nss
-        alsa-lib
-      ];
-
-      unpackPhase = "true";
-
-      installPhase = ''
-        mkdir -p $out/bin
-        dpkg -x $src $out
-        ln -s $out/opt/Freelens/freelens $out/bin/freelens
-      '';
-
-      meta = with pkgs.lib; {
-        description = "Freelens - Free IDE for Kubernetes";
-        homepage = "https://freelens.app";
-        license = licenses.mit;
-        maintainers = [];
-        platforms = [ "x86_64-linux" ];
-      };
-    };
   };
 }
